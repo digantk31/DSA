8c87f7095041871e1a9ed412dcad146398f18bf6
Digant kathiriya <105538783+digantk31@users.noreply.github.com>
2023-10-21 12:19:59 +0530

    Add files via upload

---
+++ STL/max_min_element.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // only for understanding purpose (There will be not a output of this Programm)
+
+    int maxi = *max_element(a, a + n);
+    int mini = *min_element(a, a + n);
+
+    return 0;
+}
