96f06ce5a5a3b384afc2aad0ad8526c4f428f5b2
Digant kathiriya <105538783+digantk31@users.noreply.github.com>
2023-10-21 12:15:21 +0530

    Add files via upload

---
+++ STL/__builtin_popcount.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // only for understanding purpose (There will be not a output of this Programm)
+
+    // How many 1's and sets are there in binary
+    int num1 = 7;
+    cout << __builtin_popcount(num1); // 3
+
+    long long num2 = 165786578687;
+    cout << __builtin_popcount(num2); // 21
+
+    return 0;
+}
