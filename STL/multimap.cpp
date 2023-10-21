5b958efc003b54dc72ca21a42a2f683c02f0ceb7
Digant kathiriya <105538783+digantk31@users.noreply.github.com>
2023-10-21 11:31:04 +0530

    Add files via upload

---
+++ STL/multimap.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // only for understanding purpose (There will be not a output of this Programm)
+
+    // everything same as map, only it can store multiple keys.
+    // only mpp[key] cannnot be used here
+
+    return 0;
+}
