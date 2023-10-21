76722489cea97210a731c1e09addf4ad5603eeb9
Digant kathiriya <105538783+digantk31@users.noreply.github.com>
2023-10-21 09:27:56 +0530

    Add files via upload

---
+++ STL/unordered_set.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // only for understanding purpose (There will be not a output of this Programm)
+
+    unordered_set<int> st;
+    //    lower_bound and upper_bound function does not works, rest all functions are same as multiset.
+    //  it does not stores in any particular order.
+    //  it has a better complexity [O(1)] than set in most cases, except some when collision happens. but in worst case [one in a million complexity is [O(n)].
+
+    return 0;
+}
